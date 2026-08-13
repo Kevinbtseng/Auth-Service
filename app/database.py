2ad7fba56499201b807e378e55d08763dfffc1e4
kevinbtseng <kevinbtseng@gmail.com>
2026-08-12 21:39:07 -0500

    feat: implement alembic database migrations

--- app/database.py
+++ app/database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
-import config
+import app.config
 
-engine = create_engine(config.DATABASE_URL)
+engine = create_engine(app.config.DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False, 
--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from app.database import Base
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, func
 from datetime import datetime, timezone
 
 class User(Base):
@@ -8,4 +8,4 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String(255), nullable=False, unique=True, index=True)
     hashed_password = Column(String(255), nullable=False)
-    time_created = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))  
+    time_created = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc), server_default=func.now(), nullable=False)  

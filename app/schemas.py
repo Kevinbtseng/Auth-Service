5c4d73946d84adb9776fce17181b41b9ef6f9f2e
kevinbtseng <kevinbtseng@gmail.com>
2026-08-22 19:56:15 -0500

    feat: implement user create and response schema

---
+++ app/schemas.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel, EmailStr, Field, ConfigDict
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str = Field(min_length=8)
+
+class UserResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    email: EmailStr

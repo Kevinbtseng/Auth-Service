7280eb2c7dd2b016b5af56cc3ea35622915ece61
kevinbtseng <kevinbtseng@gmail.com>
2026-08-31 17:53:42 -0500

    feat: implement access token and login route

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from app import schemas, database, crud
+from app import schemas, database, crud, security
 
 router = APIRouter(
     prefix="/auth",
@@ -14,3 +14,13 @@ async def user_signup(user: schemas.UserCreate, db: Session = Depends(database.g
     else:
         raise HTTPException(status_code=409, detail="User with the same email already exists")
 
+@router.post("/login", response_model=schemas.Token)
+async def user_login(user: schemas.UserLogin, db: Session = Depends(database.get_db)):
+    userFromEmail = crud.get_user_by_email(db, user.email)
+    if userFromEmail is None:
+        raise HTTPException(status_code=401, detail="Invalid login details")
+    elif security.verify_password(user.password, userFromEmail.hashed_password):
+        token = security.create_access_token({"sub": str(userFromEmail.id)})
+        return {"access_token": token, "token_type": "bearer"}
+    else:
+        raise HTTPException(status_code=401, detail="Invalid login details")
--- app/security.py
+++ app/security.py
@@ -1,13 +1,37 @@
 from argon2 import PasswordHasher
 from argon2.exceptions import VerifyMismatchError
+import jwt
+from datetime import datetime, timedelta, timezone
+from app import config
 
 ph = PasswordHasher()
 
 def hash_password(password: str):
     return ph.hash(password)
 
-def verify_password(plaintext_password: str, hashed_password:str):
+def verify_password(plaintext_password: str, hashed_password: str):
     try:
         return ph.verify(hashed_password, plaintext_password)
     except VerifyMismatchError:
       return False
+
+def create_access_token(data: dict, expires_delta: timedelta=None):
+    to_encode = data.copy()
+
+
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+
+    to_encode.update({
+        "exp": expire,
+        "iat": datetime.now(timezone.utc)
+    })
+
+    encoded_jwt = jwt.encode(
+        to_encode, 
+        config.JWT_SECRET, 
+        algorithm="HS256"
+    )
+    return encoded_jwt
